66ca9f053a4b20636340bed4019121af84a8c37b
antoniocruzrivera0216@gmail.com <antoniocruzrivera0216@gmail.com>
2021-10-02 20:10:36 -0500

    Avances en el método Bottom-up, reemplazo de cadena y búsqueda en las gramáticas.

--- AnalizadorSintactico/Form1.cs
+++ AnalizadorSintactico/Form1.cs
@@ -46,11 +46,109 @@ namespace AnalizadorSintactico
 
         private void btnAnalizador_Click(object sender, EventArgs e)
         {
-            int tamaño = rtxtArchivoTokens.Lines.Count();
-            //Arreglo para almacenar los tokens que hay en cada linea del archivo de tokens, para así separar trabajar individualmente con cada fila.
-            string[] arrArchivoTokens = new string[tamaño];
-            arrArchivoTokens = rtxtArchivoTokens.Lines.ToArray();
+            string instrucciones = rtxtArchivoTokens.Text.Trim();
+            //Arreglo para almacenar cada linea en una casilla diferente del arreglo para hacer la busqueda por instrucción
+            string[] arrTokens = instrucciones.Split('\n').ToArray();
+            string[] arrResultado = new string[arrTokens.Length];
+            for (int i = 0; i < arrTokens.Length; i++)
+            {
+                arrResultado[i] = BottomUp(arrTokens[i], 0);
+            }
+            string resultado = "";
+            for (int i = 0; i < arrResultado.Length; i++)
+            {
+                resultado = resultado +  arrResultado[i] + "\n";
+            }
+            rtxtResultado.Text = resultado;
+        }
+        string BottomUp(string cadena, int posicionInicial)
+        {
+            int LongitudCadena = cadena.Trim().Split(' ').ToArray().Length;
+            string cadenaActual = "";
+            //Arreglo con los tokens que contiene la cadena que se va a reducir.
+            string[] TokensCadena = cadena.Trim().Split(' ').ToArray();
+            //Si la posición actual es mayor al número de tokens se devuelve la cadena (Aquí creo que también se debe manejar lo de errores)
+            if(posicionInicial > TokensCadena.Length)
+            {
+                return cadena;
+            }
+            //Aquí se forma la cadena que se va a reducir, es decir se indica desde que número de token se va a comenzar en la cadena para cuando no se encuentre toda completa
+            for (int i = posicionInicial; i < TokensCadena.Length; i++)
+            {
+                if(i == posicionInicial)
+                {
+                    cadenaActual = cadenaActual + TokensCadena[i];
+                }
+                else
+                {
+                    cadenaActual = cadenaActual + " " + TokensCadena[i];
+                }
+            }
+            string resultado;
+            resultado = BuscarGramatica(cadenaActual);
+            //Si la busqueda en la gramática devuelve S, entonces ya se término de reducir la cadena enviada.
+            if (resultado == "S")
+            {
+                return resultado;
+            }
+            else
+            {
+                //Si el resultado regresado del metodo BuscarGramatica no es S, en el caso de que haya habido una reducción aquí se hace el reemplazo de la parte que se redujo
+                //y se vuelve a aplicar el método bottom up
+                if (resultado != cadena)
+                {
+                    cadena = ReemplazarCadena(cadena.Split(' ').ToArray(), posicionInicial, TokensCadena.Length, resultado);
+                    return (BottomUp(cadena, 0));
+                }
+                //En el caso de que la cadena no se haya reducido porque no se encoentro ninguna coincidencia, entonces se vuelve a buscar pero se
+                //mueve a la derecha la posición inicial para aplicar el método pero con un token menos.
+                else
+                {
+                    return (BottomUp(cadena, posicionInicial + 1));
+                }
+            }
+        }
+        //Método para buscar la cadena actual en la parte derecha de la gramática, si se encuentra se devuelve la parte izquierda,
+        //sino, se devuelve la misma cadena que se busco
+        string BuscarGramatica(string cadena)
+        {
+            string resultado = "";
+            using (var reader = new StreamReader("./Resources/Eq.-1-Gramáticas-libres-de-contexto.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    if(cadena == values[1])
+                    {
+                        resultado = values[0];
+                        return resultado;
+                    }
+                    else
+                    {
+                        resultado = cadena;
+                    }
+                }
+            }
+            return resultado;
+        }
+        //Método para reemplazar las partes de la cadena que ya se han encontrado en la gramatica.
+        string ReemplazarCadena(string[] cadenaActual, int posicionInicial, int posicionFinal, string subcadena)
+        {
+            string nvaCadena;
+            string subCadena1 = "";
+            string subCadena2 = "";         
+            for (int i = 0; i < posicionInicial; i++)
+            {
+                subCadena1 = subCadena1 + cadenaActual[i] + " ";
+            }
+            for (int i = posicionFinal; i < cadenaActual.Length; i++)
+            {
+                subCadena2 = subCadena2 + cadenaActual[i] + " ";
+            }
 
+            nvaCadena = subCadena1.Trim() + " " + subcadena + " " + subCadena2.Trim();
+            return nvaCadena.Trim();
         }
     }
 }
